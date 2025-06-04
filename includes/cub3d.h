94a205e439f4ff20bcbeb6a6b878866f2e5b7ca8
Alberto Bermudez <albbermu@c3a6c6.42berlin.de>
2025-06-04 14:38:24 +0200

    some structure to get going

---
+++ includes/cub3d.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albbermu <albbermu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 13:43:07 by albbermu          #+#    #+#             */
+/*   Updated: 2025/06/04 14:27:05 by albbermu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_3D
+# define CUB_3D
+
+# include "libft.h"
+# include "mlx.h"
+# include <stdio.h>
+# include <stddef.h> //size_t
+# include <stdlib.h> // exit() - free()
+# include <unistd.h> // write() - close()
+# include <stdbool.h> //bool
+# include <fcntl.h> //open() - close()
+# include <string.h>
+# include <errno.h> //strerror
+# include <signal.h> //signal
+# include <termios.h>
+# include <limits.h> //INT_MAX
+
+#endif
